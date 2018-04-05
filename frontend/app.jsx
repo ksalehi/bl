768ed60ff7c6e30a7d87db2479c2267e613145e3
Kia Salehi <ksalehi@wellesley.edu>
2018-04-05 09:16:51 -0700

    add hero image carousel

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -3,11 +3,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar.jsx';
-import $ from 'jquery';
+import Homepage from './components/Homepage.jsx';
+import AboutUs from './components/AboutUs.jsx';
 
 $(document).ready(function () {
-    ReactDOM.render(
-        <NavBar />,
-        document.getElementById('nav-bar-container')
-    );
+    console.log('path: ', window.location.pathname);
+    switch (window.location.pathname) {
+        case '/':
+            ReactDOM.render(
+                <Homepage />,
+                document.getElementById('nav-bar-container')
+            );
+            break;
+        case '/about-us':
+            ReactDOM.render(
+                <AboutUs />,
+                document.getElementById('nav-bar-container')
+            );
+            break;
+        default:
+            console.log('Page Not Found');
+            break;
+    }
+
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        dots: false,
+        autoplay: false,
+        autoplaySpeed: 3000,
+    });
+
 });
