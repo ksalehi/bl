91bc21e514723d9a340986b9c4806fa7610bbb64
Kia Salehi <ksalehi@wellesley.edu>
2018-04-15 00:41:14 -0700

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/show'
-
-  root 'home#index'
-  scope 'api' do
-      resources :blahs
-  end
-  get 'home/index'
-  get ':page' => 'static_pages#show'
+    get 'static_pages/show'
+    root 'home#index'
+    get 'home/index'
+    get ':page' => 'static_pages#show'
 end
