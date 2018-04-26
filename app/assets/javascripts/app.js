9d69810b6b0d175059ac7b885162cde881463259
Kia Salehi <ksalehi@wellesley.edu>
2018-04-25 20:16:55 -0700

    try adding app.js file to js folder

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar.jsx';
+import Homepage from './components/Homepage.jsx';
+import AboutUs from './components/AboutUs.jsx';
+
+$(document).ready(function () {
+    ReactDOM.render(
+        <NavBar />,
+        document.getElementById('nav-bar-container')
+    );
+
+    switch (window.location.pathname) {
+        case '/':
+            ReactDOM.render(
+                <Homepage />,
+                document.getElementById('home')
+            );
+            break;
+        case '/about-us':
+            ReactDOM.render(
+                <AboutUs />,
+                document.getElementById('about-us')
+            );
+            break;
+    }
+});
